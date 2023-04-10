a45a9f21adc53f4ea46a96c3c4b203217a18c542
Sam Kim <samjkim11@gmail.com>
2023-04-10 13:17:17 -0400

    Augment material ui type to include new types

---
+++ src/types/createPalette.d.ts
@@ -0,0 +1,12 @@
+import * as createPalette from "@mui/material/styles/createPalette"
+declare module "@mui/material/styles/createPalette" {
+  interface PaletteOptions {
+    neutral?: PaletteColorOptions
+  }
+  interface Palette {
+    neutral: PaletteColor
+  }
+  interface TypeBackground {
+    alt: string
+  }
+}
