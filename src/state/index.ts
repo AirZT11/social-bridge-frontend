5b52bdda1674ec7d59c31beb75cc431c03463ac1
Sam Kim <samjkim11@gmail.com>
2023-04-12 21:59:47 -0400

    Update tUser interface

--- src/state/index.ts
+++ src/state/index.ts
@@ -6,7 +6,7 @@ export interface User {
   lastName: string
   email: string
   picturePath: string
-  friends: string[]
+  friends: User[]
   location: string
   occupation: string
   viewedProfile: number
@@ -74,7 +74,7 @@ export const authSlice = createSlice({
     },
     setPost: (state, action) => {
       const updatedPosts = state.posts.map((post) => {
-        if (post._id === action.payload.post_id) return action.payload.post
+        if (post._id === action.payload.post._id) return action.payload.post
         return post
       })
       state.posts = updatedPosts
